cebe2cff137d218daf971a73e0f19c16dbdcd27d
Samuli Seppänen <samuli@openvpn.net>
2019-06-25 14:01:48 +0300

    Allow not using exported Samba HLKInstall share
    It is known to be rather fragile in the long run
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,15 +4,18 @@
 #   File name of the test certificate
 # @param testcert_source
 #   Test certificate location (on the puppet fileserver)
+# @param hlkinstall_drive_letter
+#   The drive letter of the HLKInstall network drive. For example "R". If this
+#   is defined the exported resource that would otherwise (try to)
+#   automatically mount the HLKInstall Samba share will not get mounted.
 #
 class hlk::client
 (
-  String $testcert,
-  String $testcert_source
+  String           $testcert,
+  String           $testcert_source,
+  Optional[String] $hlkinstall_drive_letter = undef,
 )
 {
-  # Mount the SMB share "HLKInstall" on the controller as user "hlkclient"
-  Mount <<| tag == 'hlk_controller' |>>
 
   # We can't use Setup.cmd with the package resource, but fortunately it is 
   # just a simple wrapper around the actual executable installer.
@@ -20,8 +23,17 @@ class hlk::client
     'x64' => 'Setupamd64.exe',
     'x86' => 'Setupx86.exe',
   }
+
+  if $hlkinstall_drive_letter {
+    $drive = $hlkinstall_drive_letter
+  } else {
+    # Mount the SMB share "HLKInstall" on the controller as user "hlkclient"
+    Mount <<| tag == 'hlk_controller' |>>
+    $drive = 'R'
+  }
+
+  $hlk_installer_path = "${drive}:\\Client\\${hlk_installer}"
   $hlk_description = 'Windows Hardware Lab Kit Client'
-  $hlk_installer_path = "R:\\Client\\${hlk_installer}"
 
   package { $hlk_description:
     ensure          => 'present',
